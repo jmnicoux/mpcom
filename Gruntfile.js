5a0a0a14633e3c62f1f0240be600b4042d094580
Jean-Marc Nicoux <jm.nicoux@moodmedia.com>
2013-09-26 11:33:56 +0200

    server listen on all cards disable automatic web browser opening

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,7 +64,7 @@ module.exports = function (grunt) {
             options: {
                 port: 9000,
                 // change this to '0.0.0.0' to access the server from outside
-                hostname: '127.0.0.1'
+                hostname: '0.0.0.0'
             },
             livereload: {
                 options: {
@@ -354,7 +354,7 @@ module.exports = function (grunt) {
 
     grunt.registerTask('server', function (target) {
         if (target === 'dist') {
-            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
         }
 
         grunt.task.run([
@@ -363,7 +363,6 @@ module.exports = function (grunt) {
             'concurrent:server',
             'neuter:app',
             'connect:livereload',
-            'open',
             'watch'
         ]);
     });
